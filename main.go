6cb5014c3e211e42085dd73b6b2f9f6ddcda2626
Sergey <89170475280@mail.ru>
2025-09-23 16:07:07 +0500

    готово

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	const (
+		USDtoEUR = 0.93
+		USDtoRUB = 93.5
+	)
+
+	EURtoRUB := (1 / USDtoEUR) * USDtoRUB
+
+	fmt.Printf("Курсы конвертации:\n")
+	fmt.Printf("USD to EUR: %.2f\n", USDtoEUR)
+	fmt.Printf("USD to RUB: %.2f\n", USDtoRUB)
+	fmt.Printf("EUR to RUB: %.2f\n", EURtoRUB)
 }
